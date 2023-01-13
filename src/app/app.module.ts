0531b5b84e7271a3b8a93e188f8e9b19b7fb7658
Richenel2 <yvanprogramme@gmail.com>
2023-01-13 11:49:02 +0100

    finish login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from './environment';
+import { AuthService } from "./shared/services/auth.service";
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDViSpZOsMB9CaualGcIzMDsjvaNvJV0HU",
+        authDomain: "nota-8b381.firebaseapp.com",
+        projectId: "nota-8b381",
+        storageBucket: "nota-8b381.appspot.com",
+        messagingSenderId: "931049927473",
+        appId: "1:931049927473:web:740e6ab73e2f5fce327610",
+        measurementId: "G-5KFBXP5QWM"
+    }
+};
