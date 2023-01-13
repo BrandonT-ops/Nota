03a6115515ebb40389fb62ce85b51032e5f59536
Richenel2 <yvanprogramme@gmail.com>
2023-01-13 14:15:57 +0100

    Finish witch firebase

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
         this.SetUserData(result.user);
         this.afAuth.authState.subscribe((user) => {
           if (user) {
-            this.router.navigate(['dashboard']);
+            this.router.navigate(['home']);
           }
         });
       })
@@ -88,7 +88,7 @@ export class AuthService {
   // Sign in with Google
   GoogleAuth() {
     return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      this.router.navigate(['dashboard']);
+      this.router.navigate(['home']);
     });
   }
   // Auth logic to run auth providers
@@ -96,7 +96,7 @@ export class AuthService {
     return this.afAuth
       .signInWithPopup(provider)
       .then((result) => {
-        this.router.navigate(['dashboard']);
+        this.router.navigate(['home']);
         this.SetUserData(result.user);
       })
       .catch((error) => {
@@ -116,6 +116,7 @@ export class AuthService {
       displayName: user.displayName,
       photoURL: user.photoURL,
       emailVerified: user.emailVerified,
+      genre:"",
     };
     return userRef.set(userData, {
       merge: true,
@@ -126,6 +127,15 @@ export class AuthService {
     return this.afAuth.signOut().then(() => {
       localStorage.removeItem('user');
       this.router.navigate(['sign-in']);
+    });
+  }
+
+  update(user:User){
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    return userRef.set(user, {
+      merge: true,
     });
   }
 }
--- src/app/shared/services/user.ts
+++ src/app/shared/services/user.ts
@@ -3,5 +3,34 @@ export interface User {
     email: string;
     displayName: string;
     photoURL: string;
+    genre:string;
     emailVerified: boolean;
+    // todoCategorie:TodoCategorie[];
+    // userEvents:UserEvent[];
+    // userNote:UserNote[];
+ }
+
+ export interface TodoCategorie{
+    title:string;
+    todos:Todo[];
+ }
+
+ export interface Todo{
+    tasK:string;
+    state:boolean;
+ }
+
+ export interface UserEvent{
+    title:string;
+    description:string;
+    date:Date;
+    reminder:Date;
+ }
+
+ export interface UserNote{
+    title:string;
+    subtitle:string;
+    contain:string;
+    createAt:Date;
+    imageUrl:string;
  }
